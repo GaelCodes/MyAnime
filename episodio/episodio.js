13368863251dae728ef48f26d888ec8d0d9eb394
Gael <abigael-hf@hotmail.com>
2021-07-10 22:54:38 +0200

    DB_Animes creado

---
+++ episodio/episodio.js
@@ -0,0 +1,9 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const anime = urlParams.get('anime').replace(/_/g, ' ').toUpperCase();
+const capitlo = urlParams.get('capitulo');
+const title = document.getElementById('title');
+
+title.innerText = `${anime} episodio ${capitlo}`
+
+var episodio;
---
+++ js/app.js
@@ -0,0 +1,59 @@
+const episodesContainer = document.getElementById('last-episodes-container');
+const lastAnimesContainer = document.getElementById('last-animes-container');
+
+const animesLastEpisodes;
+const newAnimes;
+
+
+
+// fetch();
+
+
+// let i = 1;
+// animes.forEach(
+//     anime => {
+//         if (i <= 12) {
+//             episodesContainer.innerHTML +=
+//                 `
+//                 <a href="episodio.html?anime=${anime.titulo_}&capitulo=24" class="card col-12 col-sm-6 col-md-6 col-lg-3">
+//                 <div class="aspect-ratio-thumbnail">
+//                     <img src="${anime.thumbnail}" class="card-img-top" alt="Image not found">
+
+
+//                     <div class="card-img-overlay d-flex flex-column justify-content-end">
+//                         <h5 class="card-title text-white">${anime.titulo}</h5>
+//                         <h6 class="card-subtitle mb-2 text-muted">Episodio 10.000</h6>
+//                     </div>
+//                 </div>
+
+//                 </a>
+//             `;
+//             i++;
+//         }
+
+//     }
+// );
+
+
+// animes.forEach(
+//     anime => {
+//         if (anime.reciente) {
+
+//             lastAnimesContainer.innerHTML +=
+//                 `
+
+//                 <div class="card col-12 col-sm-6 col-md-6 col-lg-3">
+//                     <div class="aspect-ratio-cartel">
+//                     <img src="assets/${anime.cartel}" class="card-img-top" alt="Image not found">
+//                     <div class="card-img-overlay d-flex flex-column justify-content-end align-items-center">
+//                         <h5 class="card-title text-white">${anime.titulo}</h5>
+//                     </div>
+//                     </div>
+//                 </div>
+
+
+//             `;
+
+//         }
+//     }
+// )
