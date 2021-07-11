6f4b615987efda0c685d06cfa8dc2b8f286c8424
Gael <abigael-hf@hotmail.com>
2021-07-11 21:31:15 +0200

    Añadiendo el CDN de consulta de los capitulos

--- episodio/episodio.js
+++ episodio/episodio.js
@@ -1,9 +1,23 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
+const anime_ = urlParams.get('anime');
 const anime = urlParams.get('anime').replace(/_/g, ' ').toUpperCase();
-const capitlo = urlParams.get('capitulo');
+const capitulo = urlParams.get('capitulo');
 const title = document.getElementById('title');
 
-title.innerText = `${anime} episodio ${capitlo}`
+title.innerText = `${anime} episodio ${capitulo}`
 
 var episodio;
+
+
+
+fetch(`https://cdn.jsdelivr.net/gh/GaelCodes/MyAnime@v0.1.2/DB_Animes/${anime_}/cap${capitulo}.json`)
+    .then(
+        data => {
+            console.log(data);
+        }
+    ).catch(
+        error => {
+            console.log(error);
+        }
+    )
