4a956b933c6f852d484e324ea2cc7498a809823c
Gael <abigael-hf@hotmail.com>
2021-07-11 22:44:39 +0200

    Lógica de obtención de datos en episodio implementada

--- episodio/episodio.js
+++ episodio/episodio.js
@@ -1,22 +1,90 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const anime_ = urlParams.get('anime');
-const anime = urlParams.get('anime').replace(/_/g, ' ').toUpperCase();
-const capitulo = urlParams.get('capitulo');
+// const anime = urlParams.get('anime').replace(/_/g, ' ').toUpperCase();
+const num = urlParams.get('capitulo');
+
+
+
 const title = document.getElementById('title');
-
-title.innerText = `${anime} episodio ${capitulo}`
-
-var episodio;
-
+const video = document.getElementById('video');
+const videoAuthor = document.getElementById('videoAuthor');
+const comments = document.getElementById('comments');
 
 
-fetch(`https://cdn.jsdelivr.net/gh/GaelCodes/MyAnime@v0.1.3/DB_Animes/${anime_}/cap${capitulo}.json`)
+// title.innerText = `${anime} episodio ${num}`;
+
+var episode;
+
+
+
+fetch(`https://cdn.jsdelivr.net/gh/GaelCodes/MyAnime@v0.1.3/DB_Animes/${anime_}/cap${num}.json`)
+    .then(response => response.json)
     .then(
         data => {
-            console.log(data);
-        }
-    ).catch(
+            episode = data;
+            // Pintar titulo y episodio
+            title.innerText = `${episode.title} episodio ${episode.num}`
+
+            // Definir video src with author
+            video.src = episode.file;
+            videoAuthor.innerText = episode.author;
+
+            // Mostrar comentarios
+            episode.comments.forEach(
+                comment => {
+
+                    comments.innerHTML =
+                        `
+                     <li>
+                        <div class="d-flex flex-row align-items-center">
+                            <div class="flex-shrink-0">
+                                <img src="${comment.picture}" alt="Image not found" class="img-fluid">
+                            </div>
+                            <div class="flex-grow-1 ms-4">
+                                <p class="m-0">${comment.author}</p>
+                                <p class="m-0">${comment.text}</p>
+                            </div>
+                        </div>
+                     </li>                        
+                        `
+
+                    if (comment.respuestas) {
+                        comments.innerHTML += '<ul class="response-list">';
+
+
+                        comment.respuestas.forEach(
+
+                            response => {
+                                comment.innerHTML +=
+
+
+                                    `
+                                    <li>
+                                        <div class="d-flex flex-row align-items-center">
+                                            <div class="flex-shrink-0">
+                                                <img src="${response.picture}" alt="Image not found" class="img-fluid">
+                                            </div>
+                                            <div class="flex-grow-1 ms-4">
+                                                <p class="m-0">${response.author}</p>
+                                                <p class="m-0">${response.text}</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    `
+                            }
+                        )
+
+                        ;
+                        comments.innerHTML += '</ul>';
+
+                    };
+
+                });
+
+
+        })
+    .catch(
         error => {
             console.log(error);
         }
