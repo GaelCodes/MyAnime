26c4ed0ae78bafb48b219c558b00283406a4d664
Abigael <abigael-hf@hotmail.com>
2022-01-28 14:52:57 +0100

    Tipo Mime añadido, template de notificaciones incorporado, charset modificada Fixes #32

--- código/backend/public/crunchyroll/crunchyrollNotifier.php
+++ código/backend/public/crunchyroll/crunchyrollNotifier.php
@@ -12,129 +12,122 @@ class CrunchyrollNotifier extends Notifier {
     protected function prepare_email($subscriber_email, $episode) {
         $prepared_email = $this->default_email;
 
-        $prepared_email['headers']['To'] = $subscriber_email;
-        $prepared_email['headers']['Subject'] = 'Hay nuevo episodio de tu anime favorito listo';
-        $prepared_email['body'] = '
+        $headers = array(
+            'From'    => 'My Anime <info@abigaelheredia.es>',
+            'To' => $subscriber_email,
+            'Subject' => 'Hay nuevo episodio de tu anime favorito listo',
+            'Content-Type' => 'text/html;charset=utf-8',
+            
+            );      
+
+        $text = 'Text version of email';
+
+        $html = '
         <!DOCTYPE html>
-        <html lang="es">
-        
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        </head>
-        
-        <body>
-        
-            <style>
-                * {
-                    max-width: 100%;
-                    box-sizing: border-box;
-                }
-                
-                body {
-                    background: linear-gradient( 0deg, rgb(26 21 40) 0%, rgb(63 55 96) 20%);
-                    color: white;
-                    margin: 0;
-                    font-family: Roboto, -apple-system, BlinkMacSystemFont, \'Segoe UI\', Oxygen, Ubuntu, Cantarell, \'Open Sans\', \'Helvetica Neue\', sans-serif;
-                }
-                /* Inicio Header */
-                
-                header {
-                    display: flex;
-                    flex-flow: row nowrap;
-                    justify-content: center;
-                    align-items: center;
-                    text-align: center;
-                    background-color: rgba(13, 0, 27, 0.678);
-                }
-                /* Fin Header */
-                /* Inicio Main */
-                
-                main section {
-                    display: flex;
-                    text-align: center;
-                    flex-flow: column nowrap;
-                    align-items: center;
-                }
-                
-                section .episodeCard {
-                    width: 50%;
-                    display: flex;
-                    flex-flow: column nowrap;
-                    justify-content: flex-end !important;
-                    align-items: center;
-                    padding: 1em;
-                    margin: 0.5em 0;
-                    border-radius: 1em;
-                    /* Only for debug */
-                    background-color: #181146;
-                    -webkit-box-shadow: 6px 4px 2px 0px #0000005c;
-                    box-shadow: 6px 4px 2px 0px #0000005c;
-                    color: gray;
-                }
-                
-                .episodeCard .episodeIndex {
-                    color: white;
-                    max-width: 100%;
-                }
-                
-                .episodeCard .episodeViewNowButton {
-                    background-color: #433699;
-                    color: white;
-                    padding: 0.6em;
-                    border-radius: 1em;
-                    border: none;
-                    text-decoration: none;
-                    width: 50%;
-                    text-align: center;
-                }
-                
-                .episodeCard .episodeViewNowButton:hover {
-                    background-color: #594f99;
-                }
-                /* Fin Main */
-                /* Inicio Footer */
-                
-                footer {
-                    padding: 5em 2em;
-                    display: flex;
-                    flex-flow: row nowrap;
-                    text-align: center;
-                    justify-content: center;
-                    background-color: rgba(0, 0, 0, 0.678);
-                    color: rgba(219, 219, 219, 0.932);
-                }
-                /* Fin Footer */
-            </style>
-        
-            <header>
-                <h1>MyAnime <br> Tu notificador de anime favorito</h1>
-            </header>
-        
-            <main>
-                <section>
-                    <h2 class="saludo"> ¡Hey! </h2>
-                    <h2> Tu anime está listo <br> ¡ Disfrútalo ! </h2>
-        
-                    <li class="episodeCard">
-                        <img class="episodeThumbnail" src="'.$episode['mediaThumbnail'][0]['@attributes']['url'].'">
-                        <p class="episodeVersionTitle">'.$episode['version'].'</p>
-                        <p class="episodeNumber">Episodio '.$episode['crunchyrollEpisodeNumber'].'</p>
-                        <a target="_blank" class="episodeViewNowButton" href="'.$episode['link'].'">Ver ahora</a>
-                    </li>
-        
-                </section>
-            </main>
-        
-            <footer>
-                <p>My Footer</p>
-            </footer>
+<html lang="es" style="margin: 0; padding: 0;">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title></title>
+
+</head>
+
+<body style="margin: 0; padding: 0;">
+    <div id="email" style="width: 100%; background: linear-gradient( 0deg, #1a1528 0%, #3f3760 20%); color: #ffffff; margin: 0; padding: 0; font-family: Roboto, -apple-system, BlinkMacSystemFont, \'Segoe UI\', Oxygen, Ubuntu, Cantarell, \'Open Sans\', \'Helvetica Neue\', sans-serif;">
+
+        <!-- Header -->
+        <table id="header" role="presentation" border="0" cellspacing="0" width="100%" style="margin: 0; background-color: #0d001bad; padding: 2em; text-align: center; color: #dbdbdbee;" bgcolor="#0d001bad" align="center">
+            <tr style="margin: 0; padding: 0;">
+                <td style="margin: 0; padding: 0;">
+                    <h1 style="margin: 0; padding: 0; font-size: 1rem;">MyAnime <br style="margin: 0; padding: 0;"> Tu notificador de anime favorito</h1>
+                </td>
+            </tr>
+        </table>
+
+        <!-- Main -->
+        <table id="main" role="presentation" border="0" cellspacing="0" width="100%" style="margin: 0; text-align: center; padding: 2em; color: #dbdbdbee;" align="center">
+            <tr style="margin: 0; padding: 0;">
+                <td style="margin: 0; padding: 0;">
+                    <h2 class="saludo" style="margin: 0; padding: 0; font-size: 1rem;"> ¡Hey! </h2>
+                    <h2 style="margin: 0; padding: 0; font-size: 1rem;"> Tu anime está listo <br style="margin: 0; padding: 0;"> ¡ Disfrútalo ! </h2>
+                </td>
+            </tr>
+
+            <tr style="margin: 0; padding: 0;">
+                <td style="margin: 0; padding: 0;">
+                    <!-- EpisodeCard Table -->
+                    <table class="episodeCard" style="width: 50%; padding: 1em; text-align: center; margin: 0.5em auto; border-radius: 1em; background-color: #181146; -webkit-box-shadow: 6px 4px 2px 0px #0000005c; box-shadow: 6px 4px 2px 0px #0000005c; color: gray;" width="50%"
+                        align="center" bgcolor="#181146">
+                        <!-- Thumbnail -->
+                        <tr style="margin: 0; padding: 0;">
+                            <td style="margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; text-align: center; padding-top: 1em;" align="center">
+                                <img class="episodeThumbnail" src="'.$episode['mediaThumbnail'][0]['@attributes']['url'].'" style="margin: 0; padding: 0; width: 100%;">
+                            </td>
+                        </tr>
+
+                        <!-- Title -->
+                        <tr style="margin: 0; padding: 0;">
+                            <td style="margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; text-align: center; padding-top: 1em;" align="center">
+                                <p class="episodeVersionTitle" style="margin: 0 auto; padding: 0; font-size: 0.8rem;">'.$episode['version'].'</p>
+                            </td>
+                        </tr>
+
+                        <!-- Episode number -->
+                        <tr style="margin: 0; padding: 0;">
+                            <td style="margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; text-align: center; padding-top: 1em;" align="center">
+                                <p class="episodeNumber" style="margin: 0 auto; padding: 0; font-size: 0.8rem;">Episodio '.$episode['crunchyrollEpisodeNumber'].'</p>
+                            </td>
+                        </tr>
+
+                        <!-- Button -->
+                        <tr style="margin: 0; padding: 0;">
+                            <td cellpadding="20%" style="margin: 0; padding: 0; display: flex; justify-content: center; align-items: center; text-align: center; padding-top: 1em;" align="center">
+                                <a target="_blank" class="episodeViewNowButton" href="'.$episode['link'].'" style="margin: 0 auto; background-color: #433699; color: white; padding: 0.6em; border-radius: 1em; border: none; text-decoration: none; width: 50%; text-align: center;">Ver ahora</a>
+                            </td>
+                        </tr>
+
+
+                    </table>
+                </td>
+            </tr>
+        </table>
+
+        <!-- Footer -->
+        <table id="footer" role="presentation" border="0" cellspacing="0" width="100%" style="margin: 0; padding: 2em; background-color: rgba(0, 0, 0, 0.678); color: #dbdbdbee; text-align: center;" bgcolor="rgba(0, 0, 0, 0.678)" align="center">
+            <tr style="margin: 0; padding: 0;">
+                <td style="margin: 0; padding: 0;">
+
+                    <p style="margin: 0; padding: 0; font-size: 0.8rem;">My Footer</p>
+
+                </td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
+       ';
+
+        $crlf = "\n";
+        $mime = new Mail_mime(array('eol' => $crlf));
         
         
-        </body>
         
-        </html>';
+        $mime->setTXTBody($text);
+        $mime->setHTMLBody($html);
+        
+        $mimeparams = [];
+        $mimeparams['text_encoding']="8bit";
+        $mimeparams['text_charset']="UTF-8";
+        $mimeparams['html_charset']="UTF-8";
+        $mimeparams['head_charset']="UTF-8";
+            
+        $prepared_email['body'] = $mime->get($mimeparams);
+
+        // Mail_Mime::headers() has to be called after Mail_Mime::get().
+        $headers = $mime->headers($headers);
+        $prepared_email['headers'] = $headers;
         return $prepared_email;
         
     }
